32095e790c0326f7aa168456d3e4490a3f6e809f
kdave97 <karandave1997@gmail.com>
2020-10-13 00:43:02 -0400

    #4 Code added for badge view

--- code/popup.js
+++ code/popup.js
@@ -1,46 +1,83 @@
-const params = {
-  active: true,
-  currentWindow: true
-}
+var params = {
+    active: true,
+    currentWindow: true
+};
 
-let message = {
-  experience: {
-    checked: true
-  },
-  sponsorship: {
-    checked: true
-  }
-}
-
-document.body.onload = function () {
-  chrome.storage.sync.get('data', function (items) {
-    if (!chrome.runtime.error) {
-      message = items.data
-      for (const badge in message) {
-        document.getElementById(badge).checked = message[badge].checked
-      }
-      chrome.tabs.query(params, gotTabs)
+var message = {
+    experience: {
+        checked: true
+    },
+    sponsorship: {
+        checked: true
     }
-  })
+};
+document.addEventListener('DOMContentLoaded', function () {
+    chrome.storage.sync.get("data", function (items) {
+        if (!chrome.runtime.error) {
+            message = items["data"];
+            for (const badge in message) {
+                document.getElementById(badge).checked = message[badge]["checked"];
+            }
+            chrome.tabs.query(params, gotTabs);
+        }
+    });
+}, false);
+
+
+function gotTabs(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, message);
 }
 
-function gotTabs (tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, message)
+function toggle(event) {
+    const badge = event.target.id;
+    message[badge]['checked'] = event.target.checked;
+    chrome.tabs.query(params, gotTabs);
+
+    chrome.storage.sync.set({"data": message}, function () {
+        if (chrome.runtime.error) {
+            console.log("Runtime error.");
+        }
+    });
 }
 
-function toggle (event) {
-  const badge = event.target.id
-  message[badge].checked = event.target.checked
-  chrome.tabs.query(params, gotTabs)
-  chrome.storage.sync.set({ data: message }, function () {
-    if (chrome.runtime.error) {
-      console.log('Runtime error.')
+document.addEventListener('DOMContentLoaded', function () {
+    const checkboxes = document.getElementsByTagName("input");
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener('change', function (event) {
+            var params = {
+                active: true,
+                currentWindow: true
+            };
+
+            var message = {
+                experience: {
+                    checked: true
+                },
+                sponsorship: {
+                    checked: true
+                }
+            };
+            const badge = event.target.id;
+            message[badge]['checked'] = event.target.checked;
+
+            chrome.tabs.query(params, gotTabs);
+
+            chrome.storage.sync.set({"data": message}, function () {
+                if (chrome.runtime.error) {
+                    console.log("Runtime error.");
+                }
+            });
+
+        });
     }
-  })
-}
+}, false);
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    const checkboxes = document.getElementsByTagName("input");
+    console.log(checkboxes.length);
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener('change', toggle);//, false);
+    }
+}, false);
+*/
 
-const checkboxes = document.getElementsByTagName('input')
-
-for (const checkbox of checkboxes) {
-  checkbox.addEventListener('change', toggle, false)
-}

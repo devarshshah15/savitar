64aa0a5628a7f7fd9e7ae08837c71fdd9c9fc690
kdave97 <karandave1997@gmail.com>
2020-10-14 01:59:35 -0400

    Hopefully fixed lint errors

--- code/popup.js
+++ code/popup.js
@@ -1,83 +1,71 @@
 var params = {
-    active: true,
-    currentWindow: true
-};
+  active: true,
+  currentWindow: true
+}
 
 var message = {
-    experience: {
-        checked: true
-    },
-    sponsorship: {
-        checked: true
-    }
-};
+  experience: {
+    checked: true
+  },
+  sponsorship: {
+    checked: true
+  }
+}
 document.addEventListener('DOMContentLoaded', function () {
-    chrome.storage.sync.get("data", function (items) {
-        if (!chrome.runtime.error) {
-            message = items["data"];
-            for (const badge in message) {
-                document.getElementById(badge).checked = message[badge]["checked"];
-            }
-            chrome.tabs.query(params, gotTabs);
-        }
-    });
-}, false);
+  chrome.storage.sync.get('data', function (items) {
+    if (!chrome.runtime.error) {
+      message = items.data
+      for (const badge in message) {
+        document.getElementById(badge).checked = message[badge].checked
+      }
+      chrome.tabs.query(params, gotTabs)
+    }
+  })
+}, false)
 
-
-function gotTabs(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, message);
+function gotTabs (tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, message)
 }
 
-function toggle(event) {
-    const badge = event.target.id;
-    message[badge]['checked'] = event.target.checked;
-    chrome.tabs.query(params, gotTabs);
+function toggle (event) {
+  const badge = event.target.id
+  message[badge].checked = event.target.checked
+  chrome.tabs.query(params, gotTabs)
 
-    chrome.storage.sync.set({"data": message}, function () {
+  chrome.storage.sync.set({ data: message }, function () {
+    if (chrome.runtime.error) {
+      console.log('Runtime error.')
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const checkboxes = document.getElementsByTagName('input')
+  for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('change', function (event) {
+      var params = {
+        active: true,
+        currentWindow: true
+      }
+
+      var message = {
+        experience: {
+          checked: true
+        },
+        sponsorship: {
+          checked: true
+        }
+      }
+      const badge = event.target.id
+      message[badge].checked = event.target.checked
+
+      chrome.tabs.query(params, gotTabs)
+
+      chrome.storage.sync.set({ data: message }, function () {
         if (chrome.runtime.error) {
-            console.log("Runtime error.");
+          console.log('Runtime error.')
         }
-    });
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    const checkboxes = document.getElementsByTagName("input");
-    for (var i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].addEventListener('change', function (event) {
-            var params = {
-                active: true,
-                currentWindow: true
-            };
-
-            var message = {
-                experience: {
-                    checked: true
-                },
-                sponsorship: {
-                    checked: true
-                }
-            };
-            const badge = event.target.id;
-            message[badge]['checked'] = event.target.checked;
-
-            chrome.tabs.query(params, gotTabs);
-
-            chrome.storage.sync.set({"data": message}, function () {
-                if (chrome.runtime.error) {
-                    console.log("Runtime error.");
-                }
-            });
-
-        });
-    }
-}, false);
-/*
-document.addEventListener('DOMContentLoaded', function() {
-    const checkboxes = document.getElementsByTagName("input");
-    console.log(checkboxes.length);
-    for (var i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].addEventListener('change', toggle);//, false);
-    }
-}, false);
-*/
-
+      })
+    })
+  }
+}, false)
